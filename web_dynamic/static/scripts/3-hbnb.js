dd18634b0ff070844a93745a2d49f0651eec0dc2
Abderrahmane369 <cszamasu@gmail.com>
2024-02-11 00:18:37 +0100

    Updated_Something

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,27 +1,59 @@
 $(function () {
-    const checked = {};
+  const checked = {};
 
-    $("input[type='checkbox']").on("change", function () {
-        if ($(this).is(":checked"))
-            checked[$(this).attr("data-id")] = $(this).attr("data-name");
-        else delete checked[$(this).attr("data-id")];
+  $("input[type='checkbox']").on('change', function () {
+    if ($(this).is(':checked'))
+      checked[$(this).attr('data-id')] = $(this).attr('data-name');
+    else delete checked[$(this).attr('data-id')];
 
-        if (Object.values(checked).length > 3) {
-            $(".amenities h4").html(
-                Object.values(checked).slice(0, 3).join(", ") + ",..."
-            );
-        } else $(".amenities h4").html(Object.values(checked).join(", "));
-    });
-	$.ajax({
-	  url: 'http://0.0.0.0:5001/api/v1/status/',
-	  method: 'GET',
-	  success: function (response) {
-	    if (response.status == 'OK') {
-              $('div#api_status').addClass('available');
-	    }
-	    else {
-	      $('div#api_status').removeClass('available');
-	    }
-	  }
-	});
+    if (Object.values(checked).length > 3) {
+      $('.amenities h4').html(
+        Object.values(checked).slice(0, 3).join(', ') + ',...'
+      );
+    } else $('.amenities h4').html(Object.values(checked).join(', '));
+  });
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/places_search/',
+    method: 'POST',
+    dataType: 'json',
+    data: JSON.stringify({}),
+    contentType: 'application/json',
+    success: function (response) {
+      for (place of places = response) {
+        console.log(place);
+        $('section.places').append(`
+                <article>
+                    <div class="title_box">
+                        <h2>${place.name}</h2>
+                        <div class="price_by_night">
+                            ${place.price_by_night}$
+                        </div>
+                    </div>
+                    <div class="information">
+                        <div class="max_guest">
+                            ${place.max_guest} Guest${
+                    place.max_guest != 1 ? "s" : ""
+                }
+                        </div>
+                        <div class="number_rooms">
+                            ${place.number_rooms} Bedroom${
+                    place.number_rooms != 1 ? "s" : ""
+                }
+                        </div>
+                        <div class="number_bathrooms">
+                            ${place.number_bathrooms} Bathroom${
+                    place.number_bathrooms != 1 ? "s" : ""
+                }
+                        </div>
+                    </div>
+                    <div class="user">
+                    </div>
+                    <div class="description">
+                        ${place.description}
+                    </div>
+                </article>
+                `);
+      }
+    }
+  });
 });
