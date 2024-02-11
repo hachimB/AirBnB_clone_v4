75796c7d5147d8748938517ad767e3af26f3ac84
Abderrahmane369 <cszamasu@gmail.com>
2024-02-11 16:30:03 +0100

    Updated_Something

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -12,54 +12,6 @@ $(function () {
       );
     } else $('.amenities h4').html(Object.values(checked).join(', '));
   });
-<<<<<<< HEAD
-
-  $.ajax({
-    url: 'http://localhost:5001/api/v1/places_search/',
-    method: 'POST',
-    dataType: 'json',
-    data: JSON.stringify({}),
-    contentType: 'application/json',
-    success: function (response) {
-      for (place of places = response) {
-        console.log(place.amenities);
-        $('section.places').append(`
-                  <article>
-                      <div class="title_box">
-                          <h2>${place.name}</h2>
-                          <div class="price_by_night">
-                              ${place.price_by_night}$
-                          </div>
-                      </div>
-                      <div class="information">
-                          <div class="max_guest">
-                              ${place.max_guest} Guest${
-                      place.max_guest != 1 ? "s" : ""
-                  }
-                          </div>
-                          <div class="number_rooms">
-                              ${place.number_rooms} Bedroom${
-                      place.number_rooms != 1 ? "s" : ""
-                  }
-                          </div>
-                          <div class="number_bathrooms">
-                              ${place.number_bathrooms} Bathroom${
-                      place.number_bathrooms != 1 ? "s" : ""
-                  }
-                          </div>
-                      </div>
-                      <div class="user">
-                      </div>
-                      <div class="description">
-                          ${place.description}
-                      </div>
-                  </article>
-                  `);
-      }
-    }
-  });
-  $('button').click(() => {
-=======
    $('button[type="button"]').click(function() {
      let amenityIds = Object.keys(checked);
    $.ajax({
@@ -107,6 +59,5 @@ $(function () {
       }
     }
    });
->>>>>>> refs/remotes/origin/master
   });
 });
